bdd39050e641d40cbf9277e3327c82bfdd44da2d
vijaykumarjakhar <vijayjakhar2010@gmail.com>
2020-12-19 09:39:40 +0530

    grpc Add new client server sum program

--- grpcClientCore/Program.cs
+++ grpcClientCore/Program.cs
@@ -9,19 +9,18 @@ namespace grpcClientCore
         static async Task Main(string[] args)
         {
             var channel = Grpc.Net.Client.GrpcChannel.ForAddress("https://localhost:5001/");
-               
             var client = new Greeter.GreeterClient(channel);
             var reply = await client.SayHelloAsync(
                               new HelloRequest { Name = "GreeterClient" });
-            var clients = new SumCalculator.SumCalculatorClient(channel);
-           
-            var responce = clients.sum(new SumRequest() { A = 30, B = 40 });
+            // var client = new ToDoProtos.ToDoService.ToDoServiceClient(channel);
             var request = new Google.Protobuf.WellKnownTypes.Empty();
-            Console.WriteLine("SUM: " + responce.Result);
+
             Console.WriteLine("Greeting: " + reply.Message);
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
-           
+            //Console.WriteLine(result.Description);
+            //Console.WriteLine(result.Status);
+            //Console.WriteLine(result.Title);
 
             Console.ReadKey();
         }
