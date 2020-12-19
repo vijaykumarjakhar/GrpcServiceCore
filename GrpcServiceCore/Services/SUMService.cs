b9a032c1315ac6bd20dc62807d2edc2c33a09dc9
vijaykumarjakhar <vijayjakhar2010@gmail.com>
2020-12-19 09:19:56 +0530

    grpcAspDotNet Core  Client Server Sum Program

---
+++ GrpcServiceCore/Services/SUMService.cs
@@ -0,0 +1,27 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcServiceCore
+{
+    public class SUMService :SumCalculator.SumCalculatorBase
+    {
+        private readonly ILogger<SUMService> _logger;
+        public SUMService(ILogger<SUMService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<SumReply> sum(SumRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new SumReply
+            {
+                Result = request.A + request.B 
+            }); ;
+
+        }
+    }
+}
--- grpcClientCore/Program.cs
+++ grpcClientCore/Program.cs
@@ -9,18 +9,19 @@ namespace grpcClientCore
         static async Task Main(string[] args)
         {
             var channel = Grpc.Net.Client.GrpcChannel.ForAddress("https://localhost:5001/");
+               
             var client = new Greeter.GreeterClient(channel);
             var reply = await client.SayHelloAsync(
                               new HelloRequest { Name = "GreeterClient" });
-            // var client = new ToDoProtos.ToDoService.ToDoServiceClient(channel);
+            var clients = new SumCalculator.SumCalculatorClient(channel);
+           
+            var responce = clients.sum(new SumRequest() { A = 30, B = 40 });
             var request = new Google.Protobuf.WellKnownTypes.Empty();
-
+            Console.WriteLine("SUM: " + responce.Result);
             Console.WriteLine("Greeting: " + reply.Message);
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
-            //Console.WriteLine(result.Description);
-            //Console.WriteLine(result.Status);
-            //Console.WriteLine(result.Title);
+           
 
             Console.ReadKey();
         }
